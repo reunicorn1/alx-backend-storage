a378f25799c124e34c21278cbfde9ab65c926ad5
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-14 01:44:27 +0400

    2-fans.sql

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,6 @@
+-- This script imports data from metal_bands.sql and ranks country
+-- origins of bands, ordered by the number of (non-unique) fans
+SELECT origin, sum(fans) as nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
