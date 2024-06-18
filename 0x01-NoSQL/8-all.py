b07c87ebb4ab5a03321f3f8f3fee5312281334ab
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-18 15:15:37 +0400

    no type hinting

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -15,6 +15,4 @@ def list_all(mongo_collection):
     a list of all documents in a collection
     or an empty list
     """
-    if not isinstance(mongo_collection, Collection):
-        return []
     return list(mongo_collection.find())
