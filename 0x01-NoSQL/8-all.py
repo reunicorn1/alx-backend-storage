828a2742163ecbc07d91b1a8c011af848db7e3b6
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-18 15:11:41 +0400

    8-all.py - return to normal state

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,18 +5,18 @@
 from typing import List
 from pymongo.collection import Collection
 
-if __name__ == "__main__":
-    def list_all(mongo_collection: Collection) -> List:
-        """
-        list_all:  lists all documents in a collection
-        -----------------
-        Parameters:
-        mongo_collection (Pymongo Collection Object)
-        -----------------
-        Returns:
-        a list of all documents in a collection
-        or an empty list
-        """
-        if not isinstance(mongo_collection, Collection):
-            return []
-        return list(mongo_collection.find())
+
+def list_all(mongo_collection: Collection) -> List:
+    """
+    list_all:  lists all documents in a collection
+    -----------------
+    Parameters:
+    mongo_collection (Pymongo Collection Object)
+    -----------------
+    Returns:
+    a list of all documents in a collection
+    or an empty list
+    """
+    if not isinstance(mongo_collection, Collection):
+        return []
+    return list(mongo_collection.find())
