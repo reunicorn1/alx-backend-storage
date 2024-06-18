e8c13c0a2406f7e0bf65b9693389497df7369fcd
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-18 14:57:34 +0400

    8-all.py

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+8. List all documents in Python
+"""
+from typing import List
+from pymongo.collection import Collection
+
+
+def list_all(mongo_collection: Collection) -> List:
+    """
+    list_all:  lists all documents in a collection
+    -----------------
+    Parameters:
+    mongo_collection (Pymongo Collection Object)
+    -----------------
+    Returns:
+        a list of all documents in a collection
+        or an empty list
+    """
+    if not isinstance(mongo_collection, Collection):
+        return []
+    return mongo_collection.find()
