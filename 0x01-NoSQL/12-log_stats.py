5e2ba4697f946a4719c172613bc76ff204abe03d
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-06-18 16:20:30 +0400

    12-log_stats.py

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+12. Log stats
+"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(collection.count_documents({}), ' logs')
+    print('Methods:')
+    for method in methods:
+        number = collection.count_documents({'method': method})
+        print(f'\tmethod {method}: {number}')
+    one_line = collection.count_documents({'method': 'GET', 'path': '/status'})
+    print(f'{one_line} status check')
